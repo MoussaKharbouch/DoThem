5e37665124b0f11e41e348f4b1d63b517f4fcdf2
MoussaDev <112768280+MoussaKharbouch@users.noreply.github.com>
2026-03-29 18:49:35 +0200

    feat(TaskItemService): add ITaskItemService interface

---
+++ DoThem.Services/ITaskItemService.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text;
+using DoThem.Domain;
+using DoThem.Infrastructure;
+
+namespace DoThem.Services;
+
+public interface ITaskItemService
+{
+
+    /// <summary>
+    /// find task by task id
+    /// </summary>
+    TaskItem? FindTask(int taskID);
+
+    /// <summary>
+    /// find task by task name and user id and task type id
+    /// </summary>
+    TaskItem? FindTask(string taskName, int userID, int taskTypeID);
+
+    /// <summary>
+    /// check if task exists by task id
+    /// </summary>
+    bool DoesTaskExist(int taskID);
+
+    /// <summary>
+    /// check if task exists by task name and user id and task type id
+    /// </summary>
+    bool DoesTaskExist(string taskName, int userID, int taskTypeID);
+
+    /// <summary>
+    /// add new task
+    /// </summary>
+    /// <returns>
+    /// the id of the new task
+    /// </returns>
+    int? AddTask(TaskItem task);
+
+    /// <summary>
+    /// update task
+    /// </summary>
+    bool UpdateTask(int taskID, TaskItem newTask);
+
+    /// <summary>
+    /// delete task
+    /// </summary>
+    bool DeleteTask(int taskID);
+
+    /// <summary>
+    /// get tasks with all fields
+    /// </summary>
+    List<TaskItem> GetTasks();
+
+    /// <summary>
+    /// get tasks by user id
+    /// </summary>
+    List<TaskItem> GetTasks(int userID);
+
+    /// <summary>
+    /// get tasks by user id and task type id
+    /// </summary>
+    List<TaskItem> GetTasks(int userID, int taskTypeID);
+
+}
--- DoThem.Services/obj/Debug/net10.0/DoThem.Services.AssemblyInfo.cs
+++ DoThem.Services/obj/Debug/net10.0/DoThem.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoThem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc59701bac9dc8a10f12705e27763c2b2411346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffaadcbde4d151ba27d82cb860d243da7ce864cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoThem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoThem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
